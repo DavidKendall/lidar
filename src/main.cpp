b6d54723e9ff23f83c84fa4011791400049e734f
David Kendall <david.kendall@northumbria.ac.uk>
2019-01-02 15:54:28 +0000

    Some minor tweaks - still flaky

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@ const uint8_t rspGetHealth[] = {0xA5, 0x5A, 0x03, 0x00, 0x00, 0x00, 0x06};
 
 UARTSerial lidar(D1, D0, 115200);
 Serial pc(USBTX, USBRX, 115200);
-uint8_t lidBuffer[128];
+uint8_t lidBuffer[32];
 rplidar_response_device_health_t health;
 rplidar_response_device_info_t info;
 measurement_buffer_t measurement[8192];
@@ -48,13 +48,16 @@ int main() {
   int32_t i;
   ssize_t result;
 
-  //result = lidar.write(comResetCore, sizeof(comResetCore));
-  //assert(result == sizeof(comResetCore));
+  // Lights out
+  red = 1;
+  green = 1;
+
+  // Say hello
   pc.printf("Lidar Client\n");
+
+  // Get health
   result = lidar.write(comGetHealth, sizeof(comGetHealth));
   assert(result == sizeof(comGetHealth));
-  red = 0;
-  green = 1;
   lidarRead(lidBuffer, sizeof(rspGetHealth));
   if (memcmp(lidBuffer, rspGetHealth, sizeof(rspGetHealth)) != 0) { 
     pc.printf("Health: Bad response\n");
@@ -69,10 +72,10 @@ int main() {
   else {
     pc.printf("Health status: Warning/Error %d\n", health.error_code);
   }
+
+  // Get info
   result = lidar.write(comGetInfo, sizeof(comGetInfo));
   assert(result == sizeof(comGetInfo));
-  red = 0;
-  green = 1;
   lidarRead(lidBuffer, sizeof(rspGetInfo));
   if (memcmp(lidBuffer, rspGetInfo, sizeof(rspGetInfo)) != 0) { 
     pc.printf("Info: Bad response\n");
@@ -91,7 +94,18 @@ int main() {
   }
 
   while (true) {
+
+    // Start the motor
     motoctl = 1;
+
+    // Make sure the receive buffer is empty before starting a new scan
+    while (lidar.readable()) {
+      lidarRead(lidBuffer, 1);
+    }
+
+    red = 0;
+
+    // Start a scan
     result = lidar.write(comStartScan, sizeof(comStartScan));
     assert(result == sizeof(comStartScan));
     lidarRead(lidBuffer, sizeof(rspStartScan));
@@ -99,14 +113,42 @@ int main() {
       pc.printf("Start scan: Bad response\n");
       assert(false);
     }
+
+    green = 1;
+
+    /* Demonstrate that, once a scan starts, it can keep going without
+     * further intervention.
+     * There's only about 70us between readings, so we don't have time for much
+     * processing here/
+     */ 
+    //i = 0;
+    //while (true) {
+      //lidarRead(measurement[0], sizeof(measurement_buffer_t));
+      //if (measurement[0][1] & 0x01) {
+	//i += 1;
+      //}
+      //if (i == 500) {
+	//green = 1 - green;
+	//i = 0;
+      //}
+    //}
+
+    // Capture some readings
     for (i = 0; i < 8192; i++) {
       lidarRead(measurement[i], sizeof(measurement_buffer_t));
     }
+
     red = 1;
     green = 0;
+
+    // Stop the scan
     result = lidar.write(comStopScan, sizeof(comStopScan));
     assert(result == sizeof(comStopScan));
+
+    // Stop the motor
     motoctl = 0;
+
+    // Print the readings
     for (i = 0; i < 8192; i++) {
       rplidar_response_measurement_node_t *m = (rplidar_response_measurement_node_t *)&(measurement[i]);
       pc.printf("%4ld: %02X %02X %02X %02X %02X\n", i, measurement[i][0],measurement[i][1],measurement[i][2],measurement[i][3],measurement[i][4]);
@@ -114,10 +156,6 @@ int main() {
       pc.printf("Quality : %d\n", m->quality >> 2);
       pc.printf("Angle   : %5.1f\n", (m->angle >> 1) / 64.0);
       pc.printf("Distance: %5.1f\n\n", m->distance  / 4.0);
-    }
-    /* Make sure the receive buffer is empty before starting again */
-    while (lidar.readable()) {
-      lidarRead(lidBuffer, 1);
     }
   }
 }
